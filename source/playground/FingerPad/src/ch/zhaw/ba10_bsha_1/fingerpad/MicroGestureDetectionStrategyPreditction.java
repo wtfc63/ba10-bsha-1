6f6ddcd0dee3a8d05d607fda5ba8ada814aa8956
julianhanhart <julianhanhart@e62a8268-1bc6-11df-96c8-59bb23a8caa6>
2010-03-09 01:19:45 +0000

    Punkte-Filter und Richtungs-Detektion für MGDSPrediction

--- source/playground/FingerPad/src/ch/zhaw/ba10_bsha_1/fingerpad/MicroGestureDetectionStrategyPreditction.java
+++ source/playground/FingerPad/src/ch/zhaw/ba10_bsha_1/fingerpad/MicroGestureDetectionStrategyPreditction.java
@@ -19,33 +19,53 @@ public class MicroGestureDetectionStrategyPreditction implements IMicroGestureDe
 			TouchPoint[] pts = new TouchPoint[points.size()];
 			pts = points.toArray(pts);
 			boolean first = true;
-			TouchPoint prev;
+			TouchPoint prev = null;
 			TouchPoint curr;
 			TouchPoint next;
 			for (int i = 0; i < pts.length; i++) {
 				curr = pts[i];
-				curr_mg.addPoint(curr);
 				if (first) {
 					first = false;
+					curr_mg.addPoint(curr);
+					prev = curr;
 				} else if (i < (pts.length - 2)) {
-					prev = pts[i - 1];
+					//prev = pts[i - 1];
 					next = pts[i + 1];
 					float pred_x = curr.x + (curr.x - prev.x);
 					float pred_y = curr.y + (curr.y - prev.y);
 					if ((Math.abs(next.x - pred_x) > predictionTolerance) 
 							&& (Math.abs(next.y - pred_y) > predictionTolerance)) {
 						curr_mg.addPoint(next);
+						prev = curr;
 						result.add(curr_mg);
+						curr_mg.setDirection(analyseMicroGestureDirection(curr_mg));
 						curr_mg = new MicroGesture();
 						first = true;
+					} else if (curr.distanceTo(prev) >= (2 * predictionTolerance)) {
+						curr_mg.addPoint(curr);
+						prev = curr;
 					}
 				} else {
+					curr_mg.addPoint(curr);
+					curr_mg.setDirection(analyseMicroGestureDirection(curr_mg));
 					result.add(curr_mg);
 				}
 			}
 		} else {
 			curr_mg = new MicroGesture(points);
 			result.add(curr_mg);
+		}
+		return result;
+	}
+	
+	private float analyseMicroGestureDirection(MicroGesture mg) {
+		float result = 0;
+		if (mg.getPoints().size() > 1) {
+			TouchPoint first  = mg.getPoints().get(0);
+			TouchPoint second = mg.getPoints().get(1);
+			float dx = first.x - second.x;
+			float dy = first.y - second.y;
+			result = (float) (StrictMath.atan2(dy, dx));// - (Math.PI / 2));
 		}
 		return result;
 	}
