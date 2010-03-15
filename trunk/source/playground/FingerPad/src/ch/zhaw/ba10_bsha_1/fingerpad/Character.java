8a1fea9e9ef2c757e182c59c61ae7a01a87326bb
julianhanhart <julianhanhart@e62a8268-1bc6-11df-96c8-59bb23a8caa6>
2010-03-15 03:39:42 +0000

    CharacterDetectionStrategy und Logging der Detections hinzugefügt

---
+++ trunk/source/playground/FingerPad/src/ch/zhaw/ba10_bsha_1/fingerpad/Character.java
@@ -0,0 +1,62 @@
+package ch.zhaw.ba10_bsha_1.fingerpad;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+public class Character {
+
+	
+	private ArrayList<MicroGesture> microGestures;
+	private char detectedCharacter;
+	private float detectionProbability;
+	
+
+	public Character() {
+		microGestures = new ArrayList<MicroGesture>();
+		detectedCharacter = '\n';
+		detectionProbability = 0;
+	}
+	
+	public Character(Collection<MicroGesture> micro_gestures) {
+		this();
+		microGestures = new ArrayList<MicroGesture>(micro_gestures);
+	}
+	
+	
+	public void addMicroGesture(MicroGesture micro_gesture) {
+		microGestures.add(micro_gesture);
+	}
+
+	public ArrayList<MicroGesture> getMicroGestures() {
+		return microGestures;
+	}
+
+
+	public char getDetectedCharacter() {
+		return detectedCharacter;
+	}
+
+	public float getDetectionProbability() {
+		return detectionProbability;
+	}
+
+	public void setDetectedCharacter(char character) {
+		this.detectedCharacter = character;
+	}
+
+	public void setDetectionProbability(float probability) {
+		this.detectionProbability = probability;
+	}
+	
+	
+	public String toString() {
+		StringBuffer result = new StringBuffer();
+		result.append((detectedCharacter != '\n') ? detectedCharacter : "none");
+		result.append(" (");
+		result.append(detectionProbability);
+		result.append(')');
+		return result.toString();
+	}
+}
